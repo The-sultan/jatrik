34467fda2d5c1ca30109ab3d853e032627bb1adb
zegunadi <diegoalejandrorodriguez@gmail.com>
2014-11-25 16:15:37 -0200

    Agrego relacion one to one Equipo a Liga

--- jatrik-core/jatrik-core/jatrik-core-ejb/src/main/java/uy/edu/fing/tsi2/jatrik/core/domain/Equipo.java
+++ jatrik-core/jatrik-core/jatrik-core-ejb/src/main/java/uy/edu/fing/tsi2/jatrik/core/domain/Equipo.java
@@ -19,216 +19,194 @@ import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-
-
 @NamedQueries({
-	@NamedQuery(name="findEquipoLibre",query="SELECT OBJECT(u) FROM Equipo u WHERE u.usuario is null ")
-	
+    @NamedQuery(name = "findEquipoLibre", query = "SELECT OBJECT(u) FROM Equipo u WHERE u.usuario is null ")
+
 })
 @Entity
-@Table(name="EQUIPOS")
+@Table(name = "EQUIPOS")
 public class Equipo implements Serializable {
 
-	
-	
+    /**
+     *
+     */
+    private static final long serialVersionUID = -7478814344336185050L;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7478814344336185050L;
+    @Id
+    @SequenceGenerator(name = "SEQ_EQUIPOS", sequenceName = "SEQ_EQUIPOS", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_EQUIPOS")
+    @Column(name = "ID")
+    private Long id;
 
-	@Id
-	@SequenceGenerator(name="SEQ_EQUIPOS",sequenceName="SEQ_EQUIPOS",allocationSize=1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator="SEQ_EQUIPOS")
-	@Column(name="ID")
-	private Long id;
-	
-	@Column(name="NOMBRE")
-	private String Nombre;
-	
-	@Column(name="FONDOS")
-	private Double fondos=0.0;
-	
-	@Column(name="ALTURA")
-	private int altura;
-	
-	@Column(name="LATITUD")
-	private double latitud;
-	
-	@Column(name="LONGITUD")
-	private double longitud;
-	
-	@Column(name="ESTADIO")
-	private String estadio;
-	
-	@OneToOne(targetEntity = Usuario.class)
-	@JoinColumn(name="USUARIO_ID", referencedColumnName="ID", nullable = true)
-	private Usuario usuario;
-	
-	@OneToOne(targetEntity = Formacion.class, cascade = CascadeType.ALL, 
-			fetch = FetchType.EAGER)
-	@JoinColumn(name="FORMACION_ID", referencedColumnName="ID")
-	private Formacion formacionActual;
-	
-	@OneToMany(fetch=FetchType.EAGER, targetEntity = Jugador.class, mappedBy = "equipo")
-	private Set<Jugador> jugadores;
-	
-	@Column(name="ULTIMO_ENTRENAMIENTO")
-	private Date ultimoEntrenamiento;
-	
-	public Date getUltimoEntrenamiento() {
-		return ultimoEntrenamiento;
-	}
+    @Column(name = "NOMBRE")
+    private String Nombre;
 
-	public void setUltimoEntrenamiento(Date ultimoEntrenamiento) {
-		this.ultimoEntrenamiento = ultimoEntrenamiento;
-	}
-	
-	
-	public Equipo() {
-		super();
-	}
+    @Column(name = "FONDOS")
+    private Double fondos = 0.0;
 
+    @Column(name = "ALTURA")
+    private int altura;
 
+    @Column(name = "LATITUD")
+    private double latitud;
 
-	public Long getId() {
-		return id;
-	}
+    @Column(name = "LONGITUD")
+    private double longitud;
 
+    @Column(name = "ESTADIO")
+    private String estadio;
 
+    @OneToOne(targetEntity = Usuario.class)
+    @JoinColumn(name = "USUARIO_ID", referencedColumnName = "ID", nullable = true)
+    private Usuario usuario;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @OneToOne(targetEntity = Formacion.class, cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER)
+    @JoinColumn(name = "FORMACION_ID", referencedColumnName = "ID")
+    private Formacion formacionActual;
 
+    @OneToMany(fetch = FetchType.EAGER, targetEntity = Jugador.class, mappedBy = "equipo")
+    private Set<Jugador> jugadores;
 
+    @Column(name = "ULTIMO_ENTRENAMIENTO")
+    private Date ultimoEntrenamiento;
 
-	public String getNombre() {
-		return Nombre;
-	}
+    @OneToOne(targetEntity = Liga.class)
+    private Liga liga;
 
+    public Date getUltimoEntrenamiento() {
+        return ultimoEntrenamiento;
+    }
 
+    public void setUltimoEntrenamiento(Date ultimoEntrenamiento) {
+        this.ultimoEntrenamiento = ultimoEntrenamiento;
+    }
 
-	public void setNombre(String nombre) {
-		Nombre = nombre;
-	}
+    public Equipo() {
+        super();
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Double getFondos() {
-		return fondos;
-	}
+    public String getNombre() {
+        return Nombre;
+    }
 
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
 
+    public Double getFondos() {
+        return fondos;
+    }
 
-	public void setFondos(Double fondos) {
-		this.fondos = fondos;
-	}
+    public void setFondos(Double fondos) {
+        this.fondos = fondos;
+    }
 
+    public int getAltura() {
+        return altura;
+    }
 
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
 
-	public int getAltura() {
-		return altura;
-	}
+    public double getLatitud() {
+        return latitud;
+    }
 
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
 
+    public double getLongitud() {
+        return longitud;
+    }
 
-	public void setAltura(int altura) {
-		this.altura = altura;
-	}
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
 
+    public String getEstadio() {
+        return estadio;
+    }
 
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
 
-	public double getLatitud() {
-		return latitud;
-	}
+    public Usuario getUsuario() {
+        return usuario;
+    }
 
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 
+    public Set<Jugador> getJugadores() {
+        return jugadores;
+    }
 
-	public void setLatitud(double latitud) {
-		this.latitud = latitud;
-	}
+    public void setJugadores(Set<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
 
+    public Formacion getFormacion() {
+        return formacionActual;
+    }
 
+    public void setFormacion(Formacion formacion) {
+        this.formacionActual = formacion;
+    }
 
-	public double getLongitud() {
-		return longitud;
-	}
+    public Formacion getFormacionActual() {
+        return formacionActual;
+    }
 
+    public void setFormacionActual(Formacion formacionActual) {
+        this.formacionActual = formacionActual;
+    }
 
+    public Liga getLiga() {
+        return liga;
+    }
 
-	public void setLongitud(double longitud) {
-		this.longitud = longitud;
-	}
+    public void setLiga(Liga liga) {
+        this.liga = liga;
+    }
 
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
 
-
-	public String getEstadio() {
-		return estadio;
-	}
-
-
-
-	public void setEstadio(String estadio) {
-		this.estadio = estadio;
-	}
-   
-	
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-
-
-	public Set<Jugador> getJugadores() {
-		return jugadores;
-	}
-
-
-
-	public void setJugadores(Set<Jugador> jugadores) {
-		this.jugadores = jugadores;
-	}
-
-	public Formacion getFormacion() {
-		return formacionActual;
-	}
-
-	public void setFormacion(Formacion formacion) {
-		this.formacionActual = formacion;
-	}
-
-	@Override
-	public int hashCode() {
-		int hash = 0;
-		hash += (id != null ? id.hashCode() : 0);
-		return hash;
-	}
-
-	@Override
-	public boolean equals(Object object) {
+    @Override
+    public boolean equals(Object object) {
 		// TODO: Warning - this method won't work in the case the id fields are
-		// not set
-		if (!(object instanceof Equipo)) {
-			return false;
-		}
-		Equipo other = (Equipo) object;
-		if ((this.id == null && other.id != null)
-				|| (this.id != null && !this.id.equals(other.id))) {
-			return false;
-		}
-		return true;
-	}
+        // not set
+        if (!(object instanceof Equipo)) {
+            return false;
+        }
+        Equipo other = (Equipo) object;
+        if ((this.id == null && other.id != null)
+                || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "uy.com.fing.tsi2.entidades.Equipo[ id=" + id + " ]";
-	}
+    @Override
+    public String toString() {
+        return "uy.com.fing.tsi2.entidades.Equipo[ id=" + id + " ]";
+    }
 
 }
